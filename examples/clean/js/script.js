651b9e5d2b1f5774831e1084b34ff1c0c48ddbeb
Slav Hadjidimitrov <slavhadji@gmail.com>
2022-07-01 11:32:20 +0300

    widget version in demo updated

--- examples/clean/js/script.js
+++ examples/clean/js/script.js
@@ -1,6 +1,6 @@
 /* global XMLHttpRequest CXBus  */
 const widgetBaseUrl = 'https://apps.mypurecloud.de/widgets/9.0/';
-const videoengagerWidgetCDN = 'https://cdn.videoengager.com/videoengager/js/videoengager.widget.js';
+const videoengagerWidgetCDN = 'https://cdn.videoengager.com/videoengager/js/1.02/videoengager.widget.js';
 const videoengagerWidgetCSSCDN = 'https://cdn.videoengager.com/examples/css/genesys-selector-wtih-callback.css';
 
 const genesysEnvList = [

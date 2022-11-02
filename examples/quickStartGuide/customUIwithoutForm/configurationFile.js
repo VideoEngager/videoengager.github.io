5ffc2e54676d423e4e2036a6e675c883cc80cf48
maamoon95 <maamoon95@hotmail.com>
2022-11-02 16:34:33 +0400

    add multiple queues in configuratio

--- examples/quickStartGuide/customUIwithoutForm/configurationFile.js
+++ examples/quickStartGuide/customUIwithoutForm/configurationFile.js
@@ -1,5 +1,6 @@
 if (!window._genesys) { window._genesys = {}; }
 if (!window._gt) { window._gt = []; }
+
 window._genesys.widgets = {
   main: {
     downloadGoogleFont: false,
@@ -27,6 +28,16 @@ window._genesys.widgets = {
         ChatFormSubmitVideo: 'Start Video',
         WebChatTitleVideo: 'Video Chat'
       }
+    },
+    transport: {
+      interactionData: {
+        routing: {
+          targetType: 'QUEUE',
+          targetAddressVideo: 'video',
+          targetAddressChat: 'chat',
+          priority: 2
+        }
+      }
     }
   },
   webchat: {
@@ -39,10 +50,74 @@ window._genesys.widgets = {
       interactionData: {
         routing: {
           targetType: 'QUEUE',
-          targetAddress: 'video_DE',
+          targetAddress: 'chat',
           priority: 2
         }
       }
+    }
+  },
+  calendar: {
+    showAvailability: false,
+    numberOfDays: 5,
+    hideUnavailableTimeSlots: false,
+    calendarHours: {
+      interval: 10,
+      allDay: {
+        openTime: '09:00',
+        closeTime: '23:59'
+      }
+    }
+  },
+  callback: {
+    dataURL: 'https://dev.videoengager.com/api/genesys/callback',
+    userData: {
+      environment: 'https://api.mypurecloud.com.au'
+    },
+    ewt: {
+      queue: 'video'
+    },
+    countryCodes: true,
+    scheduledCallback: true,
+    form: {
+      wrapper: '<table></table>',
+      inputs: [
+        {
+          id: 'cx_form_callback_firstname',
+          name: 'firstname',
+          maxlength: '100',
+          placeholder: '@i18n:callback.CallbackPlaceholderRequired',
+          label: '@i18n:callback.CallbackFirstName'
+        },
+        {
+          id: 'cx_form_callback_lastname',
+          name: 'lastname',
+          maxlength: '100',
+          placeholder: '@i18n:callback.CallbackPlaceholderRequired',
+          label: '@i18n:callback.CallbackLastName'
+        },
+        {
+          id: 'cx_form_callback_phone_number',
+          name: 'phonenumber',
+          maxlength: '14',
+          placeholder: '@i18n:callback.CallbackPlaceholderRequired',
+          label: '@i18n:callback.CallbackPhoneNumber'
+        },
+        {
+          id: 'cx_form_callback_email',
+          type: 'email',
+          name: 'customer_email',
+          maxlength: '100',
+          placeholder: '@i18n:callback.CallbackPlaceholderRequired',
+          label: 'Email'
+        }, {
+          type: 'hidden',
+          label: 'tennantId',
+          id: 'cx_form_callback_tennantId',
+          maxlength: '100',
+          name: 'tennantId',
+          value: 'test_tenant'
+        }
+      ]
     }
   },
   extensions: { VideoEngager: window.videoEngager.initExtension }

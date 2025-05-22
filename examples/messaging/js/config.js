9d8949928a146b99e35235cfacbf4c3a3c5990d5
maamoon95 <maamoon95@hotmail.com>
2025-05-22 21:41:01 +0400

    feat: add more methods for custom attributes and messages

--- examples/messaging/js/config.js
+++ examples/messaging/js/config.js
@@ -26,14 +26,24 @@
  * @type {TestConfig}
  */
 export const testConfig = {
-    videoEngager: {
-      tenantId:       '0FphTk091nt7G1W7',       // Provided by VideoEngager
-      veEnv:          'videome.leadsecure.com',  // e.g., 'videome.leadsecure.com'
-      isPopup:        false                   // true for popup video, false for inline
-    },
-    genesys: {
-      deploymentId: 'c5d801ae-639d-4e5e-a52f-4963342fa0dc', // Genesys Cloud deployment ID
-      domain:       'mypurecloud.com'       // e.g., 'mypurecloud.com', 'usw2.pure.cloud'
-    },
-    useGenesysMessengerChat: true // Set to true to enable Genesys chat functions
-  };
+  videoEngager: {
+    tenantId: '0FphTk091nt7G1W7', // Provided by VideoEngager
+    veEnv: 'videome.leadsecure.com', // e.g., 'videome.leadsecure.com'
+    isPopup: false, // true for popup video, false for inline
+    customAttributes: {}, // (Optional) Custom attributes to send to VideoEngager
+    // after setting configs, customAttributes can change by calling method setCustomAttributes
+    firstMessage: 'Hello, how can I help you today?', // (Optional) Initial message for the chat
+    // after setting configs, firstMessage can change by calling method setFirstMessage
+    attributeChangeMessage: 'Attribute changed', // (Optional) Message to send when videoEngager attempt to change attributes
+    // after setting configs, attributeChangeMessage can change by calling method setAttributeChangeMessage
+    callStartedMessage: 'Call started', // (Optional) Message to send when call is started (will fallback to attributeChangeMessage)
+    // after setting configs, callStartedMessage can change by calling method setCallStartedMessage
+    callEndedMessage: 'Call ended' // (Optional) Message to send when call is ended (will fallback to attributeChangeMessage)
+    // after setting configs, callEndedMessage can change by calling method setCallEndedMessage
+  },
+  genesys: {
+    deploymentId: 'c5d801ae-639d-4e5e-a52f-4963342fa0dc', // Genesys Cloud deployment ID
+    domain: 'mypurecloud.com' // e.g., 'mypurecloud.com', 'usw2.pure.cloud'
+  },
+  useGenesysMessengerChat: true // Set to true to enable Genesys chat functions
+};
